d3e7a5c3b864973d32e9e8db847558d265339b32
pooja48-patul <poojabadappanava89@gmail.com>
2025-04-24 11:38:37 +0530

    save in master

--- Demo.java
+++ Demo.java
@@ -4,4 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("addition of a and b:"a+b);
         System.out.println("substraction of a and b:"a-b);
+        System.out.println("multiplication of a and b:"a*b);
+        System.out.println("division of a and b:"a/b);
 
